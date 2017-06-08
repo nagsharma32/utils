5fe1e0cc5d2c4fff2eca5042b709942b5b1baa88
shravikanarayanam <shravika1994@gmail.com>
2017-06-07 22:46:19 -0700

    print arithmetic operations

---
+++ python/lpdh/ex3.py
@@ -0,0 +1,20 @@
+print "I will count my chickens:"
+
+print "Hens", 25+30/6
+
+print "roosters",100-25*3%4
+print "I will count my eggs:"
+ 
+ print 3+2+1-5+4%2-1/4+6
+ print "Is it true that3+2<5-7?"
+ print 3+2<5-7
+ print "what is 3+2?",3+2
+ print "what is 5-7?",5-7
+ print "Oh, that's why it's False."
+
+print "How about some more."
+print "is it greater?",5>-2
+print "is it greater or equal?"5>=-2
+print "is it less or equal?"5<=-2
+ 
+ 
